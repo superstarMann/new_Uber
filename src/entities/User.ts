4b618e9a473ccf4f057632ea5062c4d4406fcf40
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-08-30 22:09:03 +0900

    Hashpassword

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,41 +1,92 @@
-import {IsEmail} from "class-validator";
-import {BaseEntity, Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from "typeorm"
+import bcrypt, { hash } from "bcrypt";
+import { IsEmail } from "class-validator";
+import {
+  BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+
+const BCRYPT_ROUNDS = 10;
 
 @Entity()
 class User extends BaseEntity {
- @PrimaryGeneratedColumn() id: number;
+  @PrimaryGeneratedColumn() id: number;
 
- @Column({type: "text", unique: true})
- @IsEmail()
- email: string
+  @Column({ type: "text", unique: true })
+  
+  @IsEmail()
+  email: string;
 
- @Column({type: "boolean", default: false})
- verifiedEmail: boolean;
+  @Column({ type: "boolean", default: false })
+  verifiedEmail: boolean;
 
- @Column({type: "text"})
- firstName: string;
+  @Column({ type: "text" })
+  firstName: string;
+  
+  @Column({ type: "text" })
+  lastName: string;
+  
+  @Column({ type: "int" })
+  age: number;
+  
+  @Column({ type: "text" })
+  password: string;
+  
+  @Column({ type: "text" })
+  phoneNumber: string;
+  
+  @Column({ type: "boolean", default: false })
+  verifiedPhonenNumber: boolean;
+  
+  @Column({ type: "text" })
+  profilePhoto: string;
+  
+  @Column({ type: "boolean", default: false })
+  isDriving: boolean;
+  
+  @Column({ type: "boolean", default: false })
+  isRiding: boolean;
+  
+  @Column({ type: "boolean", default: false })
+  isTaken: boolean;
+  
+  @Column({ type: "double precision", default: 0 })
+  lastLng: number;
+  
+  @Column({ type: "double precision", default: 0 })
+  lastLat: number;
+  
+  @Column({ type: "double precision", default: 0 })
+  lastOrientation: number;
 
- @Column({type: "text"})
- lastName: string;
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
 
- @Column({type: "int"})
- age: number;
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+ 
+  public comparePassword(password: string): Promise<boolean>{
+      return bcrypt.compare(password, this.password);
+  }
 
- @Column({type: "text"})
- password: string;
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if (this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  }
 
- @Column({type: "text"})
- phoneNumber: string;
-
- @Column({type: "boolean", default: false})
- verifiedPhoneNumber: boolean;
-
- @Column({type: "text"})
- profilePhoto: string;
-
-@CreateDateColumn() createdAt: string;
-@UpdateDateColumn() updatedAt: string;
-
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUNDS);
+  }
 }
 
 export default User;
