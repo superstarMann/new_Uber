8fb6c69f46fad87d4632408e5eb0753607181ba0
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-09-01 22:14:23 +0900

    fb login sucess

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -12,10 +12,9 @@ const BCRYPT_ROUNDS = 10;
 class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
   
-  @Column({ type: "text", unique: true })
-  
+  @Column({ type: "text", nullable: true})
   @IsEmail()
-  email: string;
+  email: string | null;
   
   @Column({ type: "boolean", default: false })
   verifiedEmail: boolean;
@@ -26,13 +25,13 @@ class User extends BaseEntity {
   @Column({ type: "text" })
   lastName: string;
   
-  @Column({ type: "int" })
+  @Column({ type: "int", nullable: true})
   age: number;
   
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   password: string;
   
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   phoneNumber: string;
   
   @Column({ type: "boolean", default: false })
