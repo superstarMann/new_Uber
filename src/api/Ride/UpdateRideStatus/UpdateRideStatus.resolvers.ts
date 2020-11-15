25bca2483783c1dde422a5730c8bafd4c724240f
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-11-15 21:10:01 +0900

    20.11.15

---
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -0,0 +1,54 @@
+import { UpdateRideStatusMutationArgs, UpdateRideStatusResponse } from './../../../types/graphql.d';
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/resolverMiddleware";
+import User from '../../../entities/User';
+import Ride from '../../../entities/Ride';
+
+const resolver: Resolvers = {
+    Mutation:{
+        UpdateRideStatus: privateResolver(async(_, args: UpdateRideStatusMutationArgs, {req}): Promise<UpdateRideStatusResponse> => {
+            const user: User = req.user;
+            if (user.isDriving){
+                try{
+                    let ride: Ride | undefined;
+                    if(args.status === "ACCEPTED"){
+                        const ride = await Ride.findOne({id: args.rideId, status: "REQUESTING"});
+                    if(ride){
+                        ride.driver = user;
+                        user.isTaken = true;
+                        user.save()  
+                      }                  
+                    }else{
+                        ride = await Ride.findOne({id: args.rideId, driver: user});
+                    }
+                    if(ride){
+                        ride.status = args.status;
+                        ride.save();
+                        return{
+                            ok: true,
+                            error: null
+                        };
+                    }else{
+                        return{
+                            ok: false,
+                            error: "Can't update ride"
+                        };
+                     }
+                   }catch(error){
+                       return{
+                           ok: false,
+                           error: error.message
+                       };
+                   }
+               }else{
+                   return{
+                       ok: false,
+                       error: "You are not driving"
+                   };
+                }
+            }
+        ) 
+    }
+};
+
+export default resolvers;
